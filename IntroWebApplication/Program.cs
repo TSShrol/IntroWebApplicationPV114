c4c4c4f6b6db30522bdb390e4d0bb439608728ef
mka <mka@A23PC11>
2023-07-07 20:39:45 +0300

    modefied MapGet

--- IntroWebApplication/Program.cs
+++ IntroWebApplication/Program.cs
@@ -4,7 +4,8 @@ app.UseDefaultFiles(); //default.html, default.htm, index.html, index.htm
 app.UseStaticFiles(); // add support static files
 
 //using parametres
-app.MapGet("/contact/{name}", (string name) => $"Contact me, {name}! ");
+app.MapGet("/contact/{name}/{surname}", (string name, string surname) => $"Contact me, {name} {surname}! ");
+app.MapGet("/contact/{name}", (string name) => $"Contact me, {name} ! ");
 app.MapGet("/contact", () => "Contact me! ");
 app.MapGet("/about", () => "About me!");
 //app.MapGet("/", () => "Hello World!");
