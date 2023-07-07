fb6c7c899992f4c8e52e3722d673a5d14b00bb0e
mka <mka@A23PC11>
2023-07-07 19:45:18 +0300

    Add project files.

---
+++ IntroWebApplication/Program.cs
@@ -0,0 +1,13 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+app.UseDefaultFiles(); //default.html, default.htm, index.html, index.htm
+app.UseStaticFiles(); // add support static files
+
+//using parametres
+app.MapGet("/contact/{name}", (string name) => $"Contact me, {name}! ");
+app.MapGet("/contact", () => "Contact me! ");
+app.MapGet("/about", () => "About me!");
+//app.MapGet("/", () => "Hello World!");
+
+
+app.Run();
